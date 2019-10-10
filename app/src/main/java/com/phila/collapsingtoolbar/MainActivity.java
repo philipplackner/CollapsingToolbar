c808ee8eda2194f083257ac6c8c5dd073fcbc677
Philipp Lackner <philipp.lackner@yahoo.de>
2019-10-10 09:34:01 +0200

    Initial commit

---
+++ app/src/main/java/com/phila/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,19 @@
+package com.phila.collapsingtoolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
